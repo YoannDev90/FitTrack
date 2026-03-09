282d002b600a861ed4a478635a17064c6cbfff9f
Lucas <85549951+LuckyTheCookie@users.noreply.github.com>
2026-03-09 18:53:39 +0100

    ref to runstore

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -2,6 +2,7 @@ export { useAppStore, useEntries, useSettings, useBadges, useSportsConfig } from
 export { useGamificationStore } from './gamificationStore';
 export { useSocialStore } from './socialStore';
 export { useEditorStore } from './editorStore';
+export { useRunStore } from './runStore';
 
 // Memoized selectors for optimized re-renders
 export * from './selectors';
